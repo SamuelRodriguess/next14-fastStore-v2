d40711f082ca2eff166e549d3cbee7dbc23a1f04
samuel rodrigues <samuel.rodrigues@acct.global>
2024-07-19 17:13:51 -0300

    feat: added NavbarLinksList

--- src/components/CustomNavbar/CustomNavbar.tsx
+++ src/components/CustomNavbar/CustomNavbar.tsx
@@ -1,5 +1,36 @@
+import styles from "./CustomNavbar.module.scss";
+import {
+  NavbarLinks as UINavbarLinks,
+  NavbarLinksList,
+  NavbarLinksListItem,
+  Button,
+  Link,
+} from "@faststore/ui";
+import "@faststore/ui/src/components/molecules/NavbarLinks/styles.scss";
+
 const CustomNavbar = () => {
-  return <div>NAVBAR CUSTOM</div>;
+  const links = ["DAME", "HERRE", "BORN", "HOME", "BEAUTY"];
+
+  return (
+    <section>
+      <UINavbarLinks className={styles.CustomNavBar}>
+        <div data-fs-navbar-center>
+          <Button variant="tertiary" size="small">
+            Set location
+          </Button>
+          <NavbarLinksList>
+            {links?.map((link) => (
+              <NavbarLinksListItem key={link}>
+                <Link variant="display" href="#">
+                  {link}
+                </Link>
+              </NavbarLinksListItem>
+            ))}
+          </NavbarLinksList>
+        </div>
+      </UINavbarLinks>
+    </section>
+  );
 };
 
 export default CustomNavbar;
